92ae2062e09bd1258fcd76ba370ec5869c0ef8a5
=Hamm01 <=himanish.kochhar@gmail.com>
2017-06-04 16:13:38 +0530

    Files added to Doc and SRC

---
+++ src/main.js
@@ -0,0 +1,7 @@
+$(function(){
+	$(".body").typed({
+		strings: [
+			'Name&nbsp;: Himanish "Hammy" <br>Email &nbsp;: <a href="mailto:contactme@hammylogics.com">contactme@hammylogics.com</a> ^500 <br><br> Vision : Make Incredible Things To Make India Proud ^500 <br> Current Project : Workshop App <br>About Me  : I am Engineer. I Love to write blog on latest technology. <br>Mostly i spend my time on working or writing web codes and  playing role playing games. I expertise in web Programming and Designs.<br> I keep myself upgrading my knowledge with learning new frameworks,languages and latest technology.<br> My favourite subjects Networking and Opreating System <br><br>  Skills : <br>  Web technologies:  HTML, CSS, Java-script, jQuery, Asp.net, Php <br>Frameworks: Wordpress, .NET framework <br> Languages:  C, Java <br> Database: SQL, MySQL <br> Software Packages: Visual studio 2016, Sql Management studio, Sublime-Text <br> Version Control: Git'],
+		showCursor: false
+	});
+});
